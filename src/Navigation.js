406ad5fd4c65a7b90a7567ca71818c674436e0da
Rodrigo Durante <rodrigo.durantee@hotmail.com>
2020-06-24 20:57:01 -0500

    trying vector icons

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { AntDesign } from "@expo/vector-icons";
 
 import HomeScreen from "./screens/Home";
 import LoginScreen from "./screens/Login";
@@ -7,12 +8,45 @@ import CartScreen from "./screens/Cart";
 
 const Tab = createBottomTabNavigator();
 
+const icons = {
+  Home: {
+    lib: AntDesign,
+    name: "home",
+  },
+  Login: {
+    lib: AntDesign,
+    name: "login",
+  },
+  Cart: {
+    lib: AntDesign,
+    name: "shoppingcart",
+  },
+};
+
 export default function Navigation() {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} />
-      <Tab.Screen name="Login" component={LoginScreen} />
-      <Tab.Screen name="Cart" component={CartScreen} />
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          title: "Home",
+        }}
+      />
+      <Tab.Screen
+        name="Login"
+        component={LoginScreen}
+        options={{
+          title: "Login",
+        }}
+      />
+      <Tab.Screen
+        name="Cart"
+        component={CartScreen}
+        options={{
+          title: "Cart",
+        }}
+      />
     </Tab.Navigator>
   );
 }
